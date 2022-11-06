0d0958b32a63c4b71c7463c055ce580d9151c1fe
Beibaibaby <xuyunlongbeibai@gmail.com>
2022-11-06 14:48:38 -0500

    bash

--- mrnet/bash_raven_fair.sh
+++ mrnet/bash_raven_fair.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-nohup python train_supervised.py --device 1 --num_label_raven 5000 &
-nohup python train_supervised.py --device 2 --num_label_raven 10000 &
-nohup python train_supervised.py --device 3 --num_label_raven 20000 &
+nohup python train_mixmatch.py --early_stopping 10 --device 0 --num_label_raven 1000 &
+nohup python train_mixmatch.py --early_stopping 10 --device 1 --num_label_raven 5000 &
+nohup python train_mixmatch.py --early_stopping 10 --device 2 --num_label_raven 10000 &
+nohup python train_mixmatch.py --early_stopping 10 --device 3 --num_label_raven 20000 &
+wait
+nohup python train_simple.py --early_stopping 10 --device 0 --num_label_raven 1000 &
+nohup python train_simple.py --early_stopping 10 --device 1 --num_label_raven 5000 &
+nohup python train_simple.py --early_stopping 10 --device 2 --num_label_raven 10000 &
+nohup python train_simple.py --early_stopping 10 --device 3 --num_label_raven 20000 &
+
